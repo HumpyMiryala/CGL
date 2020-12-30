164e36718efd0171370a774125a8838fa86f8029
HumpyMiryala <72124335+HumpyMiryala@users.noreply.github.com>
2020-12-30 08:51:06 +0530

    Add files via upload

--- CGLversion/src/TestCaseJunit.java
+++ CGLversion/src/TestCaseJunit.java
@@ -8,7 +8,7 @@ public class TestCaseJunit {
 	public int[][] alive2= {{1,2},{1,3},{2,1},{3,0},{4,5}};
 
 	@Test
-	public void testCaseOne1(){
+	public void testCaseOne(){
 	GameOfLife c =new GameOfLife();	
 	String a= c.printBoard(c.createBoard(5,5,alive));
 	Assertions.assertEquals(".....\n..*..\n.**..\n*....\n.....\n",a);
@@ -18,13 +18,14 @@ public class TestCaseJunit {
 	@Test 
 	public void nextGeneration1(){
 	GameOfLife c1 =new GameOfLife();
-	String a1= c1.printBoard(a1.generateNextGeneration(c1.createBoard(5,5,alive)));
-	Assertions.assertEquals(".....\n.**..\n.**..\n.....\n.....\n",a1);
-	System.out.println("######Test case nextGeneratio is passed######");
+	NextGeneration d= new NextGeneration();
+	String a1= c1.printBoard(d.generateNextGeneration(c1.createBoard(5,5,alive),2));
+	Assertions.assertEquals(".....\n.**..\n*....\n.**..\n.....\n",a1);
+	System.out.println("######Test case nextGeneration is passed######");
 	}
 	
 	@Test
-	public void testCaseOne(){
+	public void testCaseTwo(){
 	GameOfLife c11 =new GameOfLife();
 	String a11= c11.printBoard(c11.createBoard(6,6,alive2));
 	Assertions.assertEquals("......\n..**..\n.*....\n*.....\n.....*\n......\n",a11);
@@ -32,10 +33,11 @@ public class TestCaseJunit {
 	}
 	
 	@Test
-	public void nextGeneration(){
+	public void nextGeneration2(){
 	GameOfLife c111 =new GameOfLife();
-	String a111= c111.printBoard(a111.generateNextGeneration(c111.createBoard(6,6,alive2 )));
-	Assertions.assertEquals("......\n..**..\n.**...\n......\n......\n......\n",a111);
+	NextGeneration d= new NextGeneration();
+	String a111= c111.printBoard(d.generateNextGeneration(c111.createBoard(6,6,alive2),4));
+	Assertions.assertEquals("......\n.**...\n.**...\n......\n......\n......\n",a111);
 	System.out.println("######Test case nextGeneration2 is passed######");
 	}
 	
